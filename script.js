3db07f2aaf8dbc4acae0361c6520b87bcba75f87
kuro-kazuto <galihajipambudi@gmail.com>
2025-12-27 19:18:00 +0700

    new landing page

--- script.js
+++
@@ -1,4 +0,0 @@
-// Tambahkan event listener untuk tombol "Pelajari Lebih Lanjut"
-document.querySelector('.hero button').addEventListener('click', function() {
-    alert('Anda telah mengklik tombol "Pelajari Lebih Lanjut"!');
-});
